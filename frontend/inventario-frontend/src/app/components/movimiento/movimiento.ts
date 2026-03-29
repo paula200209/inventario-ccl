bbefba009f3233a9d9e3475ff03ae8b0f37ccd6c
paula200209 <pamaragonesm@udistrital.edu.co>
2026-03-28 20:59:43 -0500

    feat: creación del proyecto Angular con estructura base y servicios: - Se inicializa el proyecto en Angular 19 con routing y SCSS - Se crean los servicios AuthService y ProductosService para consumir la API - Se implementa un guard de autenticación para proteger rutas privadas - Se configuran las rutas: login, inventario y movimiento - Se agrega la configuración de HttpClient en app.config.ts

---
+++ frontend/inventario-frontend/src/app/components/movimiento/movimiento.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movimiento',
+  imports: [],
+  templateUrl: './movimiento.html',
+  styleUrl: './movimiento.scss',
+})
+export class Movimiento {
+
+}
